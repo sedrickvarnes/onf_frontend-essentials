9509ef6c131787d49719bf887c9d76646defea34
sedrickvarnes <Sedrickvarnes@gmail.com>
2025-09-03 17:14:56 +0200

    tulla mye, fikk fikset det

--- module_2/task.js
+++ module_2/task.js
@@ -1,18 +1,26 @@
+const saturnImage = document.querySelector("img");
 
-var saturnImage = document.querySelector("img");
 saturnImage.onmouseup = function () {
-  var modal = document.getElementById("myModal");
+  const modal = document.getElementById("myModal");
   modal.style.display = "block";
 };
 
-var closeBtn = document.getElementsByClassName("close")[0];
+const openModalBtn = document.getElementById("openModalBtn");
+
+openModalBtn.onclick = function () {
+  const modal = document.getElementById("myModal");
+  modal.style.display = "block";
+};
+
+const closeBtn = document.getElementsByClassName("close")[0];
+
 closeBtn.onclick = function () {
-  var modal = document.getElementById("myModal");
+  const modal = document.getElementById("myModal");
   modal.style.display = "none";
 };
 
 window.onclick = function (event) {
-  var modal = document.getElementById("myModal");
+  const modal = document.getElementById("myModal");
   if (event.target == modal) {
     modal.style.display = "none";
   }
