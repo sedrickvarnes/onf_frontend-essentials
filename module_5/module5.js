826845887e7569cc128aab66ec0451d59680a422
sedrickvarnes <Sedrickvarnes@gmail.com>
2025-10-13 11:32:51 +0200

    module 5

---
+++ module_5/module5.js
@@ -0,0 +1,29 @@
+
+
+
+fetch('https://api.allorigins.win/get?url=' + encodeURIComponent('https://random-d.uk/api/random'))
+  .then(response => response.json())
+  .then(data => {
+    const json = JSON.parse(data.contents);
+    const container = document.getElementById('duck_container');
+
+    container.style.backgroundImage = `url('${json.url}')`;
+  })
+  .catch(err => console.error('Fikk ikke bildet:', err, ' prøve igjen senere.'));
+
+function newDuck() {
+  fetch('https://api.allorigins.win/get?url=' + encodeURIComponent('https://random-d.uk/api/random'))
+    .then(response => response.json())
+    .then(data => {
+      const json = JSON.parse(data.contents);
+      const container = document.getElementById ('new_duck');
+
+      container.style.backgroundImage = `url('${json.url}')`;
+    
+    })
+    .catch(err => console.error('Fikk ikke dette bildet heller :(', err, ' prøve igjen senere.'));
+}
+
+
+
+
