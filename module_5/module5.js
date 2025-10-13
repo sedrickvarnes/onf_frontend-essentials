44b23603c5daa490296a7efbc357f1ad3dacfeaa
sedrickvarnes <Sedrickvarnes@gmail.com>
2025-10-13 12:42:01 +0200

    added loading

--- module_5/module5.js
+++ module_5/module5.js
@@ -10,16 +10,25 @@ fetch('https://api.allorigins.win/get?url=' + encodeURIComponent('https://random
   .catch(err => console.error('Fikk ikke bildet:', err, ' prøve igjen senere.'));
 
 function newDuck() {
+  const container = document.getElementById('newDuck');
+  const loading = document.getElementById('loading');
+
+  loading.style.display = 'block';
+
   fetch('https://api.allorigins.win/get?url=' + encodeURIComponent('https://random-d.uk/api/random'))
     .then(response => response.json())
     .then(data => {
       const json = JSON.parse(data.contents);
-      const container = document.getElementById('newDuck');
-  
       container.style.backgroundImage = `url('${json.url}')`;
       container.classList.add('add-border');
+      
+      setTimeout(() => {}, 2000); // bare for å simulere lastetid
+     
+      loading.style.display = 'none';
+      
     })
-    .catch(err => console.error('Fikk ikke dette bildet heller :(', err, ' prøve igjen senere.'));
+    .catch(err => console.error('Fikk ikke dette bildet heller :(', err, ' prøve igjen senere.'))
+
 }
 
 
