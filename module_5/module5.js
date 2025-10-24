8a0daad6d545e84a77735aa0d980b2a105243350
sedrickvarnes <Sedrickvarnes@gmail.com>
2025-10-24 10:36:03 +0200

    endret på js mye, for at den skal ha async/await

--- module_5/module5.js
+++ module_5/module5.js
@@ -1,39 +1,38 @@
-
-
-fetch('https://api.allorigins.win/get?url=' + encodeURIComponent('https://random-d.uk/api/random'))
-  .then(response => response.json())
-  .then(data => {
-    const container = document.getElementById('duck_container');
+async function startDuck() {
+  try {
+    const response = await fetch('https://api.allorigins.win/get?url=' + encodeURIComponent('https://random-d.uk/api/random'));
+    const data = await response.json();
     const json = JSON.parse(data.contents);
+
+    const container = document.getElementById('duck_container');
     container.style.backgroundImage = `url('${json.url}')`;
     container.classList.add('add-border');
-  })
-  .catch(err => console.error('Fikk ikke bildet:', err, ' prøve igjen senere.'));
+  } catch (err) {
+    console.error('Fikk ikke bildet:', err, 'prøve igjen senere.');
+  }
+}
 
-
-
-// Henter nytt bilde når knappen trykkes
-function newDuck() {
+async function newDuck() {
   const container = document.getElementById('newDuck');
   const loading = document.getElementById('loading');
 
   loading.style.display = 'block';
-    
-    setTimeout(() => {
-    fetch('https://api.allorigins.win/get?url=' + encodeURIComponent('https://random-d.uk/api/random'))
-    .then(response => response.json())
-    .then(data => {
-      const json = JSON.parse(data.contents);
-      container.style.backgroundImage = `url('${json.url}')`;
-      container.classList.add('add-border');
-    })
-    .catch(err => console.error('Fikk ikke dette bildet heller :(', err, ' prøve igjen senere.'))
-    .finally(() => {
-      loading.style.display = 'none';
-    });
-    }, 2000); //2 sekunder
+
+  await new Promise(resolve => setTimeout(resolve, 2000)); // 2 sek
+
+  try {
+    const response = await fetch('https://api.allorigins.win/get?url=' + encodeURIComponent('https://random-d.uk/api/random'));
+    const data = await response.json();
+    const json = JSON.parse(data.contents);
+
+    container.style.backgroundImage = `url('${json.url}')`;
+    container.classList.add('add-border');
+  } catch (err) {
+    console.error('Fikk ikke dette bildet heller :(', err, 'prøve igjen senere.');
+  } finally {
+    loading.style.display = 'none';
+  }
 }
 
 
-
-
+startDuck();
